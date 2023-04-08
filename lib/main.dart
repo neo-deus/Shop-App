9429b116d81731e24b2e3b9048d94288f278623c
neo_deus <arpandesp7890@gmail.com>
2023-04-08 11:19:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.deepOrange,
+        ),
+        fontFamily: 'Lato',
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
