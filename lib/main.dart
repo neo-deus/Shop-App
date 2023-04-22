6289373dbc59ff1d9256cbc0aedcaf30d203e180
neo_deus <arpandesp7890@gmail.com>
2023-04-22 20:41:37 +0530

    edit screen added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import './screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -42,6 +44,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
+          UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
