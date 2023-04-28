9aafa1832d02b1557ed63d90a9049bfb8208a5f6
neo_deus <arpandesp7890@gmail.com>
2023-04-28 17:10:57 +0530

    firebase connection

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -66,16 +69,33 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    );
-    _items.add(newProduct);
-    //_items.insert(0,newProduct);  //to add at the start of the list
-    notifyListeners();
+    var url = Uri.https('first-firebase-app-bffe9-default-rtdb.firebaseio.com',
+        '/products.json');
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'price': product.price,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      //print(response);
+      //print(json.decode(response.body));
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      //_items.insert(0,newProduct);  //to add at the start of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product product) {
